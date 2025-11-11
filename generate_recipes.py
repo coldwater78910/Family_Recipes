5c5394eabe63976d4977129bbb55cc43ee607380
coldwater78910 <coldwater78910@gmail.com>
2025-11-12 00:23:17 +0200

    Auto deploy: 2025-11-11T22:23:17Z

--- generate_recipes.py
+++ generate_recipes.py
@@ -153,15 +153,15 @@ TPL = '''<!doctype html>
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>{title} • Cook Family Recipes</title>
   <link rel="stylesheet" href="styles.css">
-  <link rel="icon" href="Icon.PNG" type="image/png">
-  <link rel="apple-touch-icon" href="Icon.PNG">
+  <link rel="icon" href="Icon.jpg" type="image/jpeg">
+  <link rel="apple-touch-icon" href="Icon.jpg">
 </head>
 <body>
   <div class="container">
     <div class="recipe-header">
       <header>
         <div class="brand">
-          <img src="Icon.PNG" class="logo" alt="Cook Family Recipes logo">
+          <img src="Icon.jpg" class="logo" alt="Cook Family Recipes logo">
           <div>
             <div class="brand-title">Cook Family Recipes</div>
             <div class="brand-sub">{title}</div>
