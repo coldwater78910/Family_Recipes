c3192cdfe533cf274e53d810e32c236cfe87b236
jethrocook0703 <jethrocook0703@gmail.com>
2025-12-06 11:05:36 +0200

    Auto deploy: 2025-12-06T09:05:36Z

---
+++ favorites-config.js
@@ -0,0 +1,21 @@
+/**
+ * favorites-config.js
+ * 
+ * Edit this file to customize which recipes appear on the homepage
+ * under "Favourite Recipes". Just add the recipe titles you want to display.
+ * 
+ * Example:
+ *   const FAVORITE_RECIPES = [
+ *     "Chicken Curry",
+ *     "Simple Sandwich",
+ *     "Welsh Cakes"
+ *   ];
+ */
+
+const FAVORITE_RECIPES = [
+  "Chicken Curry",
+  "Simple Sandwich",
+  "Homemade Woolworths rice",
+  "Two minute noodle Ramen",
+  "Welsh Cakes"
+];
